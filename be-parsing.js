9ac3a8669c837797a6f648dd6f9c976d3b35cc04
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-16 17:11:56 -0500

    Initial commit

--- be-parsing.js
+++ be-parsing.js
@@ -3,4 +3,5 @@ import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
 import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP, AllProps, AP, BAP} from './ts-refs/be-a-beacon/types' */;
+/** @import {Actions, PAP, AllProps, AP, BAP} from './ts-refs/be-parsing/types' */;
+
